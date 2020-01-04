e38e27f3ce79224602ea2e5b98f15194d25b8bef
Garavirod <rodrigogarciaavila26@gmail.com>
2020-01-03 18:02:41 -0600

    Creacion de modelos Lista

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
+import { Lista } from '../models/lista.model';
 
 
 export class DeseosService {
-  listas: any [] = [];
-  constructor() { }
+  listas: Lista [] = [];
+  constructor() { 
+    console.log('Service was initializated!!!!');    
+  }
 }
